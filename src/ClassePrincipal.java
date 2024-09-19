44424e47d8ff66676dcecfae9f215a6100292c75
ttstive <estevaomaia18@gmail.com>
2024-09-18 22:36:11 -0300

    atualizações feitas necessárias,falta implementar a ClassePrincipal.java, que será feita pelo Dimitri Kael

---
+++ src/ClassePrincipal.java
@@ -0,0 +1,10 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class ClassePrincipal {
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+    }
+}
